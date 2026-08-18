dd335ae4d2e0ae14387a9c372281e093b85b70ab
Chayanon Chantapanth <chayanon.1547@gmail.com>
2026-08-18 17:31:59 +0700

    feat: add PersonalTaskPage component with header and footer layout

--- frontend/src/pages/PersonalTask/PersonalTaskPage.jsx
+++ frontend/src/pages/PersonalTask/PersonalTaskPage.jsx
@@ -4,11 +4,11 @@ import Footer from "../../components/Footer";
 
 const PersonalTaskPage = () => {
   return (
-    <div>
+    <div className="min-h-screen bg-[#153648]">
       <Header />
-      <main>
-        <h3>Personal Task Page</h3>
-      </main>
+      <div className="flex justify-center items-center min-h-[calc(100vh-130px)]">
+        <h3 className="text-2xl text-white font-bold">PersonalTaskPage</h3>
+      </div>
       <Footer />
     </div>
   );
